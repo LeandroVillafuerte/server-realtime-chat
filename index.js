eb1611328b389c22484284f421a0d14c09c72223
Leandro <leandrovillafu@gmail.com>
2023-05-17 16:22:42 -0300

    fix socket

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ io.on("connection", (socket) => {
   socket.on("send-msg", (data) => {
     const sendUserSocket = onlineUsers.get(data.to);
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-receive", data.message);
+      socket.to(sendUserSocket).emit("msg-receive", data);
     }
   });
   socket.on("contact-added", (data) => {
