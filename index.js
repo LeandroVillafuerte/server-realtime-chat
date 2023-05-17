ae1349efc4b79ff5f956fe53aedf1202554db90c
Leandro <leandrovillafu@gmail.com>
2023-05-17 17:04:20 -0300

    cors 3

--- index.js
+++ index.js
@@ -35,11 +35,8 @@ const server = app.listen(process.env.PORT || 5000, () => {
 const io = new Server(server, {
   cors: {
     origin:"https://leandrovillafuerte.github.io/app-realtime-chat",
-    methods: ["GET", "POST"],
-    credentials: true,
   },
 });
-
 
 global.onlineUsers = new Map();
 
